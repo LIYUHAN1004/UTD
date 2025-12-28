8e388d7ef8bac3e136287a1a345411c48b81c085
s411278036@gm.ntpu.edu.tw <s411278036@gm.ntpu.edu.tw>
2025-12-28 15:22:57 +0800

    準備部署到 Zeabur:新增靜態檔案管理與 Whitenoise 設定

--- config/asgi.py
+++ config/asgi.py
@@ -1,19 +1,8 @@
 # config/asgi.py
-import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
-django_asgi_app = get_asgi_application()
-
-from apps.uma.routing import websocket_urlpatterns
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-    }
-)
+application = get_asgi_application()
